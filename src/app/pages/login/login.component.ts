593692968d61d8ee5146374c13516e59965dd4b5
João Amaral Santos <joaoamsantos@deloitte.pt>
2022-08-28 19:57:21 +0100

    Added password input and centered elements.

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  public password: string;
+  public isPasswordSet: string;
+  public errorMessage: string;
 
-  constructor() { }
+  constructor(
+    private _router:Router
+  ){
 
-  ngOnInit(): void {
   }
 
+  clickHandler(event: Event): void{
+    console.log('password: ', this.password)
+
+    if(!this.password) {
+      this.errorMessage= 'Insira uma password válida'
+      return
+    }
+
+    if(this.password?.length < 3) {
+      this.errorMessage = 'Password must have at least 3 characters'
+      return;
+    }
+
+    this._router.navigate(['../main']);
+  }
 }
