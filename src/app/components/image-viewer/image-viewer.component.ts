567b1ede6e9194507ccdbf7a7bd5c83602c8197c
João Amaral Santos <joaoamsantos@deloitte.pt>
2022-09-27 00:43:50 +0100

    Added navigation with keyboard

--- src/app/components/image-viewer/image-viewer.component.ts
+++ src/app/components/image-viewer/image-viewer.component.ts
@@ -1,8 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Store } from '@ngxs/store';
 import { ImagesActions } from 'src/app/store/images/images.actions';
 import { ImagesState } from 'src/app/store/images/images.state';
 import { ImageType } from '../image/image.component';
+
+export enum KEY_CODE {
+  RIGHT_ARROW = 39,
+  LEFT_ARROW = 37
+}
 
 
 @Component({
@@ -28,6 +33,18 @@ export class ImageViewerComponent implements OnInit {
     })
   }
 
+  @HostListener('document:keydown', ['$event'])
+    onKeyDown(event: KeyboardEvent){
+
+    if (event.keyCode === KEY_CODE.RIGHT_ARROW && this.selectedImage.id < 313) {
+      this.nextImage();
+    }
+
+    if (event.keyCode === KEY_CODE.LEFT_ARROW && this.selectedImage.id > 1) {
+      this.previousImage();
+    }
+  }
+
   previousImage():void {
     this._store.dispatch(new ImagesActions.SelectPreviousImage);
 
@@ -36,4 +53,5 @@ export class ImageViewerComponent implements OnInit {
   nextImage():void {
     this._store.dispatch(new ImagesActions.SelectNextImage);
   }
+
 }
